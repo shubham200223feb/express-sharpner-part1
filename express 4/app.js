78f9e57fb5a1b2b24d6a23d40106d3c5e12dfd35
shubham200223feb <shubham2101861@gmail.com>
2025-07-09 18:10:17 +0530

    Add files via upload

---
+++ express 4/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.get("/orders", (req, res) => {
+  res.send("Here is the list of all orders.");
+});
+
+app.post("/orders", (req, res) => {
+  res.send("A new order has been created.");
+});
+
+app.get("/users", (req, res) => {
+  res.send("Here is the list of all users.");
+});
+
+app.post("/users", (req, res) => {
+  res.send("A new user has been added.");
+});
+
+// app.use("*", (req, res) => {
+//   res.status(404).send("<h1>404 - Page Not Found</h1>");
+// });
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
